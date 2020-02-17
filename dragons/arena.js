f2fe416eff508e67f01dfd597646e4e705960ea4
JamieVaughn <wxv6@case.edu>
2020-02-17 00:29:46 -0500

    Fix dragon Arena, lifting state up into parent

--- dragons/arena.js
+++ dragons/arena.js
@@ -1,38 +1,44 @@
-const mountNode = document.getElementById('app');
+const arena = document.getElementById('app');
 
 function Arena() {
-    const [select, setSelect] = React.useState('water')
-    const dragons = [
-        {name: 'water', count: 0}, 
-        {name: 'fire', count: 0},
-        {name: 'air', count: 0},
-        {name: 'sand', count: 0},
-        {name: 'ice', count: 0}
-    ]
+    const [active, setActive] = React.useState({name: 'water', count: 0})
+    const [dragons, setDragons] = React.useState([
+      {name: 'water', count: 0}, 
+      {name: 'fire', count: 0},
+      {name: 'air', count: 0},
+      {name: 'sand', count: 0},
+      {name: 'ice', count: 0}
+  ])
 
-    const handleClick = (e) => {
-        console.log(e.target)
-        setSelect(e.target.innerText)
+    const handleClick = (d) => {
+        console.log(d)
+        setActive(d)
+    }
+    const increment = dragon => {
+      // setDragons(
+      //   dragons.map(d => d.name == dragon.name ?
+      //   {name: dragon.name, count: dragon.count += 1} : d)
+      // )
+      setDragons(
+        dragons.map(d => (d.count += d.name == dragon.name, d))
+      )
     }
 
     return (
         <div>
             <List  dragons={dragons} handler={handleClick}></List>
-            <Dragon key={select.name} name={select.name} count={select.count}/>
+            <Dragon dragon={active} clicker={increment}/>
         </div>
     )
 }
 
 function List(props) {
   console.log(props)
-  const [active, setActive] = React.useState('water')
-
   return (
     <div>
         <ul>
             {props.dragons.map((i, index) => (
-            // <li key={index} onClick={()=>setActive(i)}>{i}</li>
-            <li key={index} onClick={props.handler}>{i.name}</li>
+            <li key={index} onClick={()=>props.handler(i)}>{i.name}</li>
             )
             )}
         </ul>
@@ -41,13 +47,13 @@ function List(props) {
 }
 
 function Dragon (props) {
-  const [count, setCount] = React.useState(0)
+  console.log(props)
   return (
-    <div className="dragon" onClick={() => setCount(count +1)}>
-      <h1>{props.name}</h1>
-      <div>🐉 {count}</div>
+    <div className="dragon" onClick={() => props.clicker(props.dragon)}>
+      <h1>{props.dragon.name}</h1>
+      <div>🐉 {props.dragon.count}</div>
     </div>
   )
 }
 
-ReactDOM.render(<Arena/>, mountNode)
+ReactDOM.render(<Arena />, arena)
