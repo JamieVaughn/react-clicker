cf2f1291ef52530f2f06c9d2d1e73dedc12d1817
JamieVaughn <wxv6@case.edu>
2020-02-17 00:14:58 -0500

    Fix state issue for dragon solution

--- dragons/app.js
+++ dragons/app.js
@@ -1,32 +1,49 @@
 const list = document.getElementById('list');
-var dragons = ['water', 'fire', 'air', 'sand', 'ice']
+const list2 = document.getElementById('list2');
+var dragons = [
+  {name: 'water', count: 0}, 
+  {name: 'fire', count: 0}, 
+  {name: 'air', count: 0}, 
+  {name: 'sand', count: 0}, 
+  {name: 'ice', count: 0}
+]
 
 
 function List(props) {
-  console.log(props)
-  const [active, setActive] = React.useState('water')
+  const [dragons, setDragons] = React.useState(props.dragons)
+  const [active, setActive] = React.useState(props.dragons[0])
 
+  const increment = dragon => {
+    // setDragons(
+    //   dragons.map(d => d.name == dragon.name ?
+    //   {name: dragon.name, count: dragon.count += 1} : d)
+    // )
+    setDragons(
+      dragons.map(d => (d.count += d.name == dragon.name, d))
+    )
+  }
   return (
     <div>
         <ul>
-            {props.dragons.map((i, index) => (
-            <li key={index} onClick={() => setActive(i)}>{i}</li>
+            {dragons.map((i, index) => (
+            <li key={index} onClick={()=>setActive(i)}>{i.name}</li>
             )
             )}
         </ul>
-        <Dragon key={active} name={active} />
+        <Dragon dragon={active} clicker={active => increment(active)} />
     </div>
   )
 }
 
 function Dragon (props) {
-  const [count, setCount] = React.useState(0)
+  // console.log(props)
   return (
-    <div key={props.name} className="dragon" onClick={() => setCount(count +1)}>
-      <h1>{props.name}</h1>
-      <div>🐉 {count}</div>
+    <div className="dragon" onClick={() => props.clicker(props.dragon)}>
+      <h1>{props.dragon.name}</h1>
+      <div>🐉 {props.dragon.count}</div>
     </div>
   )
 }
 
 ReactDOM.render(<List dragons={dragons}/>, list)
+// ReactDOM.render(<List dragons={dragons}/>, list2)
